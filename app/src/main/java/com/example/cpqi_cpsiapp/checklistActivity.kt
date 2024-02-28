f05ba2346832ba51c78a34b7ceeae88ed0ffefc5
mwisemarierose <mwisemarierose@gmail.com>
2024-02-28 14:22:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cpqi_cpsiapp/checklistActivity.kt
@@ -0,0 +1,10 @@
+package com.example.cpqi_cpsiapp
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChecklistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.checklistpage)
+    }
+}
