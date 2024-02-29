d04efe97c774b0e01ea657a01458a5c194961416
mwisemarierose <mwisemarierose@gmail.com>
2024-02-29 14:24:20 +0200

    Add new audit page

--- app/src/main/java/com/example/tnsapp/AddnewActivity.kt
+++ app/src/main/java/com/example/tnsapp/AddnewActivity.kt
@@ -1,11 +1,26 @@
 package com.example.tnsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddnewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addnew)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val bodyButton = findViewById<Button>(R.id.bodyButton)
+
+        bodyButton.setOnClickListener {
+            openCategoryActivity()
+        }
+    }
+
+    private fun openCategoryActivity() {
+        startActivity(Intent(this, CategoriesActivity::class.java))
     }
 }
